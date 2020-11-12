41bd9c6ec07f8916a4958cb260ceb3f29d876e26
steve Linz <steve.linz@gmail.com>
2020-11-11 19:22:02 -0600

    lab 8 part 1

--- validation.js
+++ validation.js
@@ -1,22 +1,54 @@
 $(document).ready(function () {
     // Object containing the validation rules
     var myRules =
-        {};
+        {
+            firstName:
+                {
+                    required: true
+                },
+                grade:{
+                min: 5,
+                    max:8,
+                    digits: true
+                }
+
+
+
+        };
 
     // Object containing the error messages
     var myMessages =
-        {};
+        {
+            firstName:
+                {
+                    required: "Oh no"
+                },
+            grade:{
+                min: "too small",
+                max:"yoo big",
+                digits: "whole numbers"  }
+
+
+
+        };
 
     // Pass the configuration to the form's validate() method
     // Needs submitHandler, rules, and messages properties
     $("form").validate(
-        {}
+        {
+            submitHandler: runMyProgram,
+            rules: myRules,
+            messages: myMessages
+        }
     );
 
     function runMyProgram() {
         // Change the text of the <p> with ID of "message" to
         // state, for example, "You have registered Jane for grade 6 camp!"
         // Use the name and grade the user provided in the form.
+        var name = $("#firstName").val();
+        var grade = parseInt($("#grade").val());
+        $("p").text(`${name} at grade ${grade}`);
 
 
     }
